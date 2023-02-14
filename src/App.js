6f0ccbe5ac38efc6273396732997aa4482b73af0
Gurubasavaraja Reddy  C <gurreddy1@WKMZT206D776.global.publicisgroupe.net>
2023-02-14 18:38:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React , {useState,useCallback} from 'react'
 import './App.css';
+import {useSelector} from 'react-redux'
+import ListComponent from './ListComponent'
+import ProductEdit from './EditProductName'
+import AddProduct from './AddProduct'
 
 function App() {
+  const gList=useSelector((state)=>state)
+const [isShow,setIsShow] = useState(false)
+const [id,setId] = useState("")
+
+const onShowModalHandler=useCallback((flag,id)=>()=>{
+  setIsShow(flag)
+  setId(id)
+},[])
+const  selectedProduct=React.useMemo(()=>gList.filter((d)=>{if(d.id===id)return d})[0],[id])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddProduct />
+      <div className='listContainer'>
+     <ListComponent list={gList} onShowHandler={onShowModalHandler}/>
+     </div>
+     <div className='editContainer'>
+      {isShow && <ProductEdit  id={id} editedProduct={selectedProduct} /> }
+     </div>
     </div>
   );
 }
+
+
 
 export default App;
